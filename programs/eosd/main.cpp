678b63fe9517b33b28e625530607824333bf18b2
Matias Romeo <matias.romeo@gmail.com>
2017-06-05 21:43:09 -0300

    Enable chain_api_plugin + native_system_contract_plugin

--- programs/eosd/main.cpp
+++ programs/eosd/main.cpp
@@ -2,7 +2,12 @@
 
 #include <eos/producer_plugin/producer_plugin.hpp>
 #include <eos/chain_api_plugin/chain_api_plugin.hpp>
+<<<<<<< 1f06acf08838db94a2bfee2f1e839d05134186b6
 #include <eos/net_plugin/net_plugin.hpp>
+=======
+#include <eos/http_plugin/http_plugin.hpp>
+#include <eos/native_system_contract_plugin/native_system_contract_plugin.hpp>
+>>>>>>> Enable chain_api_plugin + native_system_contract_plugin
 
 #include <fc/log/logger_config.hpp>
 #include <fc/exception/exception.hpp>
@@ -18,7 +23,9 @@ int main(int argc, char** argv)
       app().register_plugin<net_plugin>();
       app().register_plugin<chain_api_plugin>();
       app().register_plugin<producer_plugin>();
-      if(!app().initialize<net_plugin, chain_plugin>(argc, argv))
+      app().register_plugin<http_plugin>();
+
+      if(!app().initialize<chain_plugin, chain_api_plugin, net_plugin>(argc, argv))
          return -1;
 
       app().startup();
